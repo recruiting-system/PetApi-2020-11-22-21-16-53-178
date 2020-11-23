d67e290fbb50cc3537dd9909590b3847bc38ced9
Shishi Zhang <SZhang42@slb.com>
2020-11-23 19:29:37 +0800

    refactor: comment: remove some comment to run the pipeline

--- PetApi/Startup.cs
+++ PetApi/Startup.cs
@@ -22,7 +22,7 @@ namespace PetApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
